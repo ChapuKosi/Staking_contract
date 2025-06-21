e6951e042e74482d0e27058b75daea0621f47507
_ChapuKosi <bhanujangid0212@gmail.com>
2025-06-21 13:06:15 +0000

    Add Foundry test CI

--- src/RewardToken.sol
+++ src/RewardToken.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract RewardToken is ERC20, Ownable {
     constructor() ERC20("Reward Token", "RWD") Ownable(msg.sender) {}
 
-
     // Only the staking contract can mint RWD
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
